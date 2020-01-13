0702869d2f6ce9607d0b56449ec5c873e7df263d
Antonio <antonio.marchante.hernan@gmail.com>
2020-01-13 13:44:05 +0100

    Se añaden comentarios

--- Programación Multimedia y Dispositivos Móviles/Tema 1/Ejercicios/Soluciones/CuentaBancaria/Program.cs
+++ Programación Multimedia y Dispositivos Móviles/Tema 1/Ejercicios/Soluciones/CuentaBancaria/Program.cs
@@ -1,38 +1,51 @@
 ﻿using System;
 
 namespace CuentaBancaria {
+    // Clase que ejecuta el programa principal
     class Program {
         static void Main(string[] args) {
+            // Se crea una nueva cuenta bancaria
             var account = new BankAccount("Pedro Andrés Mancebo", 1000);
 
+            // Se muestra la informacion por pantalla
             Console.WriteLine($"La cuenta {account.Number} creada por " +
                 $"{account.Owner} con un saldo inicial de {account.Balance} .");
 
+            // Se hace una retirada de dinero
             account.MakeWithdrawal(500, DateTime.Now, "Pago del alquiler");
+            // Se muestra por consola el saldo de la cuenta
             Console.WriteLine(account.Balance);
+            // Se hace un deposito en la cuenta
             account.MakeDeposit(100, DateTime.Now, "Ingreso nómina");
+            // Se muestra por consola el saldo de la cuenta
             Console.WriteLine(account.Balance);
 
-            // Test that the initial balances must be positive.
+            // Comprueba que el saldo de la cuenta se debe inicializar positivamente
             try {
+                // Se crea una nueva cuenta, con un saldo inicial negativo
                 var invalidAccount = new BankAccount("Incorrecto", -55);
             }
             catch (ArgumentOutOfRangeException e) {
+                // Se muestra el error por pantalla
                 Console.WriteLine("Error al crear una cuenta con saldo negativo.");
                 Console.WriteLine(e.ToString());
             }
 
-            // Test for negative balance.
+            // Se comprueba que una cuenta no puede quedarse con saldo negativo
             try {
+                // Se crea una retirada de dinero de la cuenta
                 account.MakeWithdrawal(750, DateTime.Now, "Intento de descubierto.");
             }
             catch (InvalidOperationException e) {
+                // Si el saldo llega a negativo, se muestra el error por pantalla
                 Console.WriteLine("Error al intentar un descubierto.");
                 Console.WriteLine(e.ToString());
             }
 
+            // Se muestra el historico de transacciones de la cuenta
             Console.WriteLine(account.GetAccountHistory());
 
+            // Se finaliza el programa
             Console.WriteLine("\nPulsa una tecla para finalizar");
             Console.ReadLine();
         }
--- Programación Multimedia y Dispositivos Móviles/Tema 1/Ejercicios/Soluciones/CuentaBancaria/clases/Transaction.cs
+++ Programación Multimedia y Dispositivos Móviles/Tema 1/Ejercicios/Soluciones/CuentaBancaria/clases/Transaction.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CuentaBancaria {
+    // Clase que define las propiedades y metodos que atañen a una transaccion
     class Transaction {
+        // Variable cantidad. También se implementa un método get para acceder a esta propiedad
         public decimal Amount { get; }
+
+        // Variable Fecha. También se implementa un método get para acceder a esta propiedadd
         public DateTime Date { get; }
+
+        // Variable descripcion. También se implementa un método get para acceder a esta propiedad
         public string Notes { get; }
 
+        // Constructor. Crea una nueva transaccion cuando se le pasa una cantidad, fecha y descripcion
         public Transaction(decimal amount, DateTime date, string note) {
             this.Amount = amount;
             this.Date = date;
