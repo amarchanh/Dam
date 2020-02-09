a6196b52be8f9931e0b19e7b13a8ee9c84911670
Antonio <antonio.marchante.hernan@gmail.com>
2020-02-09 20:47:12 +0100

    PSP - Actividades y Apuntes

---
+++ Programación de Servicios y Procesos/Workspace-PSP/Tema1/src/ejemplos/Ejemplo1.java
@@ -0,0 +1,11 @@
+package ejemplos;
+import java.io.IOException;
+
+public class Ejemplo1 {
+   public static void main(String[] args) throws IOException  {	   
+	   ProcessBuilder pb = new ProcessBuilder("NOTEPAD");
+	   Process p = pb.start();
+
+   }
+}//Ejemplo1
+
